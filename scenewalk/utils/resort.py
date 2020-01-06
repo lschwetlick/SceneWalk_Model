2b71f9866ba28c27fc0ccd4818e3d016de139fa3
Lisa Schwetlick <lisaschwetlick@gmail.com>
2020-01-06 14:51:03 +0100

    added option for sorting orson jobs

--- scenewalk/utils/resort.py
+++ scenewalk/utils/resort.py
@@ -28,9 +28,16 @@ def main():
                         "--dry_run",
                         help="just print upload commands",
                         action="store_true")
+    parser.add_argument("-o",
+                        "--orson", help="orson cluster log files",
+                        action="store_true")
     args = parser.parse_args()
     move = not args.dry_run
-    move_files(args.id,args.max_vp, move)
+    orson = args.orson
+    if orson:
+        move_files_orson(args.id, args.max_vp, move)
+    else:
+        move_files(args.id,args.max_vp, move)
 
     print("Done!")
 
@@ -84,6 +91,51 @@ def move_files(estim_id, nvp, move):
             else:
                 print("move " + rf + " to " + est)
 
+def move_files_orson(estim_id, nvp, move):
+    print("orson")
+    estim_folder_list = glob.glob("estim_"+estim_id+"*[0-9]")
+    estim_folder_list_sorted = [None for el in range(nvp)]
+    i=0
+    for p in estim_folder_list:
+        #print(p)
+        i+=1
+        mat = re.search(r"vp([0-9]*)", p)
+        vpnr = int(mat.group(1))
+        #print(vpnr)
+        if estim_folder_list_sorted[vpnr] is not None:
+            print("You seem to have 2 estims for the same subject")
+        estim_folder_list_sorted[vpnr] = p
+
+    print("liiist", estim_folder_list_sorted)
+    logs_l = glob.glob("est_slurm*.out")
+    print(logs_l)
+
+    for l in logs_l:
+        if not l:
+            print("There is no .log file for for subject")
+        else:
+            with open(l) as f:
+                first_line = f.readline()
+                vpnr = int(first_line[4:])
+            print(vpnr)
+
+            logpath = l
+            errlogpath = l[:-4]+".err"
+            est = estim_folder_list_sorted[vpnr-1]
+            if move:
+                pass
+                shutil.move(logpath, est)
+                shutil.move(errlogpath, est)
+            else:
+                print("move " + logpath + " to " + est)
+                print("move " + errlogpath + " to " + est)
+            
+            resfiles = glob.glob("estim_*vp"+str(vpnr-1)+"_*")
+            for rf in resfiles:
+                if move:
+                    shutil.move(rf, est)
+                else:
+                    print("move " + rf + " to " + est)
 if __name__ == "__main__":
     print("main")
     main()
