fixed bug where normalization was wrong for omp and added omp as an estimateable para,

scenewalk/scenewalk_model_object.py
@@ -180,6 +180,8 @@ class scenewalk:
             id_str += ", with logged z"
         if self.saclen_shift:
             id_str += ", with eta=saclen"
+        if self.omp != "off":
+            id_str += ", with omp"
         return id_str
 
     def get_params(self):
@@ -209,6 +211,9 @@ class scenewalk:
         if self.estimate_times:
             p_list["tau_pre"] = self.tau_pre
             p_list["tau_post"] = self.tau_post
+        if self.omp != "off":
+            p_list["chi"] = self.chii
+            p_list["ompfactor"] = self.ompfactor
         return p_list
 
     def get_param_list_order(self):
@@ -238,6 +243,9 @@ class scenewalk:
         if self.estimate_times:
             param_names.extend(["tau_pre"])
             param_names.extend(["tau_post"])
+        if self.omp != "off":
+            param_names.extend(["chi"])
+            param_names.extend(["ompfactor"])
         return param_names
 
     def update_params(self, scene_walk_parameters):
@@ -287,6 +295,9 @@ class scenewalk:
             if self.estimate_times:
                 self.tau_pre = scene_walk_parameters["tau_pre"]
                 self.tau_post = scene_walk_parameters["tau_post"]
+            if self.omp != "off":
+                self.chii = scene_walk_parameters["chi"]
+                self.ompfactor = scene_walk_parameters["ompfactor"]
         elif isinstance(scene_walk_parameters, list):
             scene_walk_params = scene_walk_parameters.copy()
             self.omegaAttention = scene_walk_params.pop(0)
@@ -331,6 +342,9 @@ class scenewalk:
             if self.estimate_times:
                 self.tau_pre = scene_walk_params.pop(0)
                 self.tau_post = scene_walk_params.pop(0)
+            if self.omp != "off":
+                self.chii = scene_walk_params.pop(0)
+                self.ompfactor = scene_walk_params.pop(0)
             if len(scene_walk_params) != 0:
                 warnings.warn("You passed more parameters than your model can use. Why would you do that?")
                 return scene_walk_params
@@ -882,10 +896,11 @@ class scenewalk:
         u = self.combine(map_att, map_inhib)
 
         if self.omp == "add":
+            #print("adding omp")
             #u = u * (1 * self.make_om_potential(fixs_x[1], fixs_y[1]))
             #ompfactor = 2
             omp_map = self.make_om_potential(fixs_x[1], fixs_y[1])
-            omp_map = omp_map/sum(omp_map)
+            omp_map = omp_map/np.sum(omp_map)
             # additive OMP
             u = u + (self.ompfactor * omp_map)
         if self.omp == "mult":

scenewalk/utils/utils.py
@@ -152,9 +152,12 @@ def save2pd_overall_point_estimates(chains_list, all_vp_list, def_args, priors, 
         else:
             dict1 = {"param_name":param_name, "mpde":def_args[param_name], "interv":np.nan, "left": np.nan, "right": np.nan}
         rows_list.append(dict1)
-    rows_list.append({"param_name":"tau_pre", "mpde":sw.tau_pre, "interv":np.nan, "left": np.nan, "right": np.nan})
-    rows_list.append({"param_name":"tau_post", "mpde":sw.tau_post, "interv":np.nan, "left": np.nan, "right": np.nan})
-    rows_list.append({"param_name":"foR_size", "mpde":sw.foR_size, "interv":np.nan, "left": np.nan, "right": np.nan})
+    rows_list.append({"param_name": "tau_pre", "mpde": sw.tau_pre, "interv": np.nan, "left": np.nan, "right": np.nan})
+    rows_list.append({"param_name": "tau_post", "mpde": sw.tau_post, "interv": np.nan, "left": np.nan, "right": np.nan})
+    rows_list.append({"param_name": "foR_size", "mpde": sw.foR_size, "interv": np.nan, "left": np.nan, "right": np.nan})
+    rows_list.append({"param_name": "chi", "mpde": sw.chii, "interv": np.nan, "left": np.nan, "right": np.nan})
+    rows_list.append({"param_name": "psi", "mpde": sw.ompfactor, "interv": np.nan, "left": np.nan, "right": np.nan})
+    
     hpde_df = pd.DataFrame(rows_list)
     hpde_df.to_csv(fname)
     return(hpde_df)